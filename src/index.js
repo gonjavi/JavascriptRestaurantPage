e59e06d163445f1209558312ccc3766a9d274282
gonjavi <javiercaliescali@gmail.com>
2020-02-10 17:46:01 -0500

    line brean style

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import navBar from './navbar';
 import mainSection from './mainSection';
 import myFooter from './footer';
 
-
 function component() {
   const myContent = document.getElementById('content');
-
   myContent.append(navBar());
   myContent.appendChild(mainSection());
   myContent.appendChild(myFooter());
-
-
   return myContent;
 }
 document.body.appendChild(component());
